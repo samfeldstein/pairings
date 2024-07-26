dcdb6ded41896ce59179cb8fc9a91fb12c279083
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-07-26 18:07:06 -0500

    Store saints locally
    Now when you add a saint, it will still be there when you next open the app.

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
-import { saints } from "./lists.js";
 import { getRandomElements } from "./functions.js";
 import { renderArrayAsList } from "./functions.js";
+
+let saints = [
+  "Joan of Arc",
+  "Thomas Aquinas",
+  "Edith Stein",
+  "Saint Nikolas",
+  "Saint Francis of Assisi",
+  "Saint Teresa of Avila",
+  "Saint Augustine",
+  "Saint Therese of Lisieux",
+  "Saint John Paul II",
+  "Saint Patrick",
+  "Saint Catherine of Siena",
+  "Saint Francis Xavier",
+  "Saint Ignatius of Loyola",
+  "Saint Bernadette",
+  "Saint Joseph",
+  "Saint Mary Magdalene",
+  "Saint Peter",
+  "Saint Paul",
+  "Saint Matthew",
+  "Saint Mark",
+  "Saint Luke",
+  "Saint John",
+  "Saint James",
+  "Saint Jude",
+  "Saint Andrew",
+  "Saint Bartholomew",
+  "Saint Philip",
+  "Saint Simon",
+  "Saint Matthias",
+  "Saint Stephen",
+];
 
 const saintsListEl = document.querySelector("#saints-list");
 const generateBtn = document.querySelector("#generate");
@@ -9,8 +41,9 @@ const numberOfStudentsInput = document.querySelector("#number-of-students");
 const newSaintInput = document.getElementById("new-saint");
 const allSaintsList = document.getElementById("all-saints");
 const pairingsHeading = document.getElementById("pairings");
+const localSaints = JSON.parse(localStorage.getItem("saints"));
 
-// Generate Pairings
+// Generate random saints list
 generateBtn.onclick = () => {
   const randomSaints = getRandomElements(saints, numberOfStudentsInput.value);
 
@@ -38,14 +71,20 @@ resetBtn.addEventListener("click", (event) => {
 newSaintInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     saints.push(newSaintInput.value);
+    saints.sort(); // Sort the saints array alphabetically
+    localStorage.setItem("saints", JSON.stringify(saints));
     allSaintsList.innerHTML = "";
-    renderArrayAsList(saints.sort(), allSaintsList);
+    renderArrayAsList(saints, allSaintsList); // Render the sorted saints array
     newSaintInput.value = "";
   }
 });
 
-renderArrayAsList(saints.sort(), allSaintsList);
+if (localSaints) {
+  saints = localSaints;
+}
 
-// If a list has already been generated, clear that list before rendering the new one
+renderArrayAsList(saints, allSaintsList);
 
 // If the number entered is greater than the number of array elements, allow duplicates
+
+// Allow deleting saints
