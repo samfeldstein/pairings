75e8b5873735c794c8011fcc73794de977e96103
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-08-21 09:47:25 -0500

    Fix rogue submit behavior
    Problem was with the inputs. Had to prevent default behavior on "Enter." Otherwise they were running submit automatically. So any on the Save (submit) button was running early.

--- scripts/_newList.js
+++ scripts/_newList.js
@@ -1,9 +1,9 @@
 // Create new list
 const createListBtn = document.getElementById("create-list-btn");
+const form = document.getElementById("new-list-form");
 
 createListBtn.onclick = function (event) {
-  const newListForm = document.getElementById("new-list-form");
-  newListForm.classList.toggle("hidden");
+  form.classList.toggle("hidden");
   const newListName = document.getElementById("new-list-name-input");
   this.classList.toggle("hidden");
   newListName.focus();
@@ -12,12 +12,14 @@ createListBtn.onclick = function (event) {
 // Add new list name
 const nameInput = document.getElementById("new-list-name-input");
 const listNameEl = document.getElementById("new-list-name");
+const newListContainer = document.getElementById("new-list-container");
 
-nameInput.addEventListener("keydown", function (event) {
-  const newListContainer = document.getElementById("new-list-container");
+nameInput.onkeydown = function (event) {
   const itemInput = document.getElementById("new-list-item");
 
   if (event.key === "Enter" && nameInput.value.trim() !== "") {
+    // If this isn't here, the code in the save button runs on "Enter."
+    event.preventDefault();
     listNameEl.textContent = nameInput.value.trim();
     nameInput.value = "";
     newListContainer.classList.toggle("hidden");
@@ -25,7 +27,7 @@ nameInput.addEventListener("keydown", function (event) {
     itemInput.classList.toggle("hidden");
     itemInput.focus();
   }
-});
+};
 
 // Add new list item
 const itemInput = document.getElementById("new-list-item");
@@ -34,6 +36,8 @@ const listArray = [];
 
 itemInput.onkeydown = function (event) {
   if (event.key === "Enter" && itemInput.value.trim() !== "") {
+    // If this isn't here, the code in the save button runs on "Enter."
+    event.preventDefault();
     const item = document.createElement("li");
 
     item.textContent = itemInput.value.trim();
@@ -54,7 +58,9 @@ saveButton.onclick = function (event) {
 
   // Save new list array to local storage
   // Seems to be saving before I click. Possibly need to prevent the form's default behavior?
+  // *Update: I tried prevent form's default submit behavior, and no dice. I also tried prevent the input's submit behavior.
   localStorage.setItem(`${listNameEl.textContent}`, JSON.stringify(listArray));
+  console.log("Is this thing on?");
 
   // Move to lists section
   // Clear new list container
