9bdcb2a4aedf9493c63488ddb3c82b0530ec780c
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-08-21 09:27:19 -0500

    Add create list button to new list partial

--- scripts/_newList.js
+++ scripts/_newList.js
@@ -1,14 +1,24 @@
-const nameInput = document.getElementById("new-list-listName");
+// Create new list
+const createListBtn = document.getElementById("create-list-btn");
+
+createListBtn.onclick = function (event) {
+  const newListForm = document.getElementById("new-list-form");
+  newListForm.classList.toggle("hidden");
+  const newListName = document.getElementById("new-list-name-input");
+  this.classList.toggle("hidden");
+  newListName.focus();
+};
+
+// Add new list name
+const nameInput = document.getElementById("new-list-name-input");
+const listNameEl = document.getElementById("new-list-name");
 
 nameInput.addEventListener("keydown", function (event) {
-  const listName = document.createElement("div");
   const newListContainer = document.getElementById("new-list-container");
   const itemInput = document.getElementById("new-list-item");
 
   if (event.key === "Enter" && nameInput.value.trim() !== "") {
-    listName.classList.add("list-title");
-    listName.textContent = nameInput.value.trim();
-    newListContainer.prepend(listName);
+    listNameEl.textContent = nameInput.value.trim();
     nameInput.value = "";
     newListContainer.classList.toggle("hidden");
     nameInput.classList.toggle("hidden");
@@ -17,28 +27,34 @@ nameInput.addEventListener("keydown", function (event) {
   }
 });
 
-// On enter, add a new item to the new list
+// Add new list item
 const itemInput = document.getElementById("new-list-item");
-const list = document.getElementById("new-list");
-const newList = [];
+const listEl = document.getElementById("new-list");
+const listArray = [];
 
 itemInput.onkeydown = function (event) {
   if (event.key === "Enter" && itemInput.value.trim() !== "") {
     const item = document.createElement("li");
 
     item.textContent = itemInput.value.trim();
-    list.appendChild(item);
-    newList.push(item.textContent);
+    listEl.appendChild(item);
+    listArray.push(item.textContent);
+    console.log("LIST ARRAY");
+    console.log(listArray);
+
     itemInput.value = "";
   }
 };
 
-// Save button
+// Save new list
 const saveButton = document.getElementById("save-new-list");
 
 saveButton.onclick = function (event) {
   event.preventDefault(); // Prevent page refresh
-  localStorage.setItem("newList", JSON.stringify(newList));
+
+  // Save new list array to local storage
+  // Seems to be saving before I click. Possibly need to prevent the form's default behavior?
+  localStorage.setItem(`${listNameEl.textContent}`, JSON.stringify(listArray));
 
   // Move to lists section
   // Clear new list container
