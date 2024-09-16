9d0d3ba4b44eb50b274e402f5b52d21e072e73b8
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-09-16 09:51:58 -0600

    Update service worker
    Take a network-first approach to navigation. All other resources are served from the cache.

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,13 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 
+const cacheVersion = "v1";
+
 const stuffToCache = [
   "/",
   "/index.html",
   "/about.html",
+  "/styles/_fonts.css",
+  "/styles/_reset.css",
   "/styles/global.css",
   "/scripts/app.js",
   "/fonts/BeachmanScript.woff",
@@ -21,21 +25,49 @@ self.addEventListener("install", function (event) {
   event.waitUntil(addToCache(stuffToCache));
 });
 
-// Cache-first
-self.addEventListener("fetch", function (event) {
-  event.respondWith(cacheFirst(event.request));
+// Network-first
+self.addEventListener("fetch", (event) => {
+  // Check if this is a navigation request
+  if (event.request.mode === "navigate") {
+    // Open the cache
+    event.respondWith(
+      caches.open(cacheVersion).then((cache) => {
+        // Go to the network first
+        return fetch(event.request.url)
+          .then((fetchedResponse) => {
+            cache.put(event.request, fetchedResponse.clone());
+
+            return fetchedResponse;
+          })
+          .catch(() => {
+            // If the network is unavailable, get
+            return cache.match(event.request.url);
+          });
+      })
+    );
+    // Serve everything else from the cache
+  } else {
+    event.respondWith(cacheFirst(event.request));
+  }
 });
 
 // Functions
 async function addToCache(stuff) {
-  const cache = await caches.open("v1");
+  const cache = await caches.open(cacheVersion);
   await cache.addAll(stuff);
 }
 
 async function cacheFirst(request) {
-  const cacheResponse = await caches.match(request);
-  if (cacheResponse) {
-    return cacheResponse;
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
   }
-  return fetch(request);
+  const responseFromNetwork = await fetch(request);
+  putInCache(request, responseFromNetwork.clone());
+  return responseFromNetwork;
+}
+
+async function putInCache(request, response) {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
 }
